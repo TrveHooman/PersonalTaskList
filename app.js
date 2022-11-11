5d3e2342d72c77a578990f9acacdd7df3df417d7
TrveHooman <irangaming@gmail.com>
2022-11-11 20:23:25 +0330

    added functionality to adding new tasks and the clear tasks btn

--- app.js
+++ app.js
@@ -10,22 +10,44 @@ loadEventListeners();
 
 function loadEventListeners() {
   taskForm.addEventListener('submit', addTask);
-  filterTasksInput.addEventListener('keydown', filterTasks);
   clearBtn.addEventListener('click', clearTasks);
+  filterTasksInput.addEventListener('keydown', filterTasks);
 }
 
 // *Adding EventListeners' Functions
 function addTask(e) {
-  const li = document.createElement('li');
-  // li.className('list-group-item);
   if(taskInput.value === '') {
     alert('Please enter a task below');
   } else {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between');
+    li.innerHTML = taskInput.value;
     taskList.appendChild(li);
+    
+    const remLink = document.createElement('a');
+    remLink.href = "#";
+    const remBtn = document.createElement('i');
+    remLink.appendChild(remBtn);
+    remBtn.classList.add('bi', 'bi-x');
+    
+    const doneLink = document.createElement('a');
+    doneLink.href = "#";
+    const doneBtn = document.createElement('i');
+    doneLink.appendChild(doneBtn);
+    doneBtn.classList.add('bi', 'bi-check');
+    
+    buttonDiv = document.createElement('div');
+    buttonDiv.appendChild(remLink);
+    buttonDiv.appendChild(doneLink);
+    li.appendChild(buttonDiv);
+    
+    e.preventDefault();
   }
-  
-
   e.preventDefault();
+}
+
+function clearTasks() {
+  taskList.innerHTML = '';
 }
 
 
