712a6a703ff463418117fb32ec95728af9681dda
TrveHooman <irangaming@gmail.com>
2022-11-12 20:07:31 +0330

    added task filter functionality

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // *adding Slectors
 const taskInput = document.querySelector('#newTask');
 const taskForm = document.querySelector('#taskForm');
-const filterTasksInput = document.querySelector('#filterTasks');
-const clearBtn = document.querySelector('#clearTasks');
 const taskList = document.querySelector('#tasks');
+const filterInput = document.querySelector('#filterTasks');
+const clearBtn = document.querySelector('#clearTasks');
 
 // *adding Event Listeners
 loadEventListeners();
@@ -11,7 +11,7 @@ loadEventListeners();
 function loadEventListeners() {
   taskForm.addEventListener('submit', addTask);
   clearBtn.addEventListener('click', clearTasks);
-  filterTasksInput.addEventListener('keydown', filterTasks);
+  filterInput.addEventListener('keyup', filterTasks);
 }
 
 // *Adding EventListeners' Functions
@@ -19,26 +19,32 @@ function addTask(e) {
   if(taskInput.value === '') {
     alert('Please enter a task below');
   } else {
+    // *li element
     const li = document.createElement('li');
-    li.classList.add('list-group-item', 'd-flex', 'justify-content-between');
+    li.id = 'listElement';
+    li.classList.add('list-group-item', 'position-relative');
     li.innerHTML = taskInput.value;
     taskList.appendChild(li);
     
+    // *buttons
     const remLink = document.createElement('a');
     remLink.href = "#";
     const remBtn = document.createElement('i');
     remLink.appendChild(remBtn);
     remBtn.classList.add('bi', 'bi-x');
+    remBtn.addEventListener('click', removeTask);
     
     const doneLink = document.createElement('a');
     doneLink.href = "#";
     const doneBtn = document.createElement('i');
     doneLink.appendChild(doneBtn);
     doneBtn.classList.add('bi', 'bi-check');
+    doneBtn.addEventListener('click', removeTask);
     
     buttonDiv = document.createElement('div');
     buttonDiv.appendChild(remLink);
     buttonDiv.appendChild(doneLink);
+    buttonDiv.classList.add('position-absolute', 'top-50','end-0', 'translate-middle');
     li.appendChild(buttonDiv);
     
     e.preventDefault();
@@ -48,6 +54,23 @@ function addTask(e) {
 
 function clearTasks() {
   taskList.innerHTML = '';
+}
+
+function removeTask(e) {
+  e.target.parentElement.parentElement.parentElement.remove();
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('#listElement').forEach(element => {
+    const item = element.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  });
 }
 
 
